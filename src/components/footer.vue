400dfa702ab927bffc773ad0591bf8a4697970d3
wangcc <1053578651@qq.com>
2022-08-23 10:10:12 +0800

    增加页脚

---
+++ src/components/footer.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Description: 
+ * @Author: wangcc
+ * @Date: 2022-08-23 10:07:07
+ * @LastEditors: wangcc
+ * @LastEditTime: 2022-08-23 10:07:08
+ * @FilePath: \jungehousing\src\components\footer.vue
+ * @Copyright: Copyright (c) 2016~2022 by wangcc, All Rights Reserved. 
+-->
+<template>
+<div>
+
+</div>
+</template>
+
+<script>
+export default {
+  name:'footer',
+  data(){
+   return {
+
+   }
+  }
+}
+</script>
+
+<style  lang='scss' scoped>
+
+</style>
