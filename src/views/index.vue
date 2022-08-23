e415f966ff40cd13d8387ed3a4f7c37cf941446c
wangcc <1053578651@qq.com>
2022-08-23 15:52:02 +0800

    首页编辑

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,23 +3,43 @@
  * @Author: wangcc
  * @Date: 2022-08-23 10:11:24
  * @LastEditors: wangcc
- * @LastEditTime: 2022-08-23 14:42:32
+ * @LastEditTime: 2022-08-23 15:49:24
  * @FilePath: \jungehousing\src\views\index.vue
  * @Copyright: Copyright (c) 2016~2022 by wangcc, All Rights Reserved. 
 -->
 <template>
-  <div>首页</div>
+  <div>
+    <el-carousel height="443px">
+      <el-carousel-item v-for="(item,index) in imageList" :key="index">
+        <img class="images" :src="item.url" alt="">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: 'index',
   data() {
-    return {};
+    return {
+      imageList:[
+        {
+          id: 1,
+          url:require('@/assets/images/banner1.png')
+        },
+        {
+          id: 2,
+          url:require('@/assets/images/banner2.jpg')
+        }
+      ]
+    };
   }
 };
 </script>
 
 <style  lang='scss' scoped>
+// .images{
+//   width: 100%;
+//   height: 100%;
+// }
 </style>
