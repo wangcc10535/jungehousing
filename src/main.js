f770206bdcbc3e544878c0f9c3f6d2e52053bb4a
wangcc <1053578651@qq.com>
2022-11-02 15:50:29 +0800

    x新增视频播放器组件

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Author: wangcc 1053578651@qq.com
  * @Date: 2022-09-21 21:56:02
  * @LastEditors: wangcc 1053578651@qq.com
- * @LastEditTime: 2022-10-24 15:28:16
+ * @LastEditTime: 2022-11-02 15:23:20
  * @FilePath: \jungehousing\src\main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -22,6 +22,17 @@ import '@/assets/css/iconfont/iconfont.css'
 import Pagination from "@/components/Pagination";
 import Viewer from 'v-viewer'
 import 'viewerjs/dist/viewer.css' // 引入css
+import VideoPlayer from 'vue-video-player'
+
+// 引入方式一
+import 'vue-video-player/src/custom-theme.css'
+import 'video.js/dist/video-js.css'
+
+// 引入方式二
+require('video.js/dist/video-js.css')
+require('vue-video-player/src/custom-theme.css')
+
+Vue.use(VideoPlayer)
 Vue.component('Pagination', Pagination)
 Vue.component('breadcrumb', breadcrumb)
 Vue.component('navDrawer', navDrawer)
--- src/views/about/about.vue
+++ src/views/about/about.vue
@@ -2,8 +2,8 @@
  * @Description: 
  * @Author: wangcc
  * @Date: 2022-08-23 15:13:46
- * @LastEditors: wangcc
- * @LastEditTime: 2022-08-23 15:16:24
+ * @LastEditors: wangcc 1053578651@qq.com
+ * @LastEditTime: 2022-11-02 15:34:56
  * @FilePath: \jungehousing\src\views\about\about.vue
  * @Copyright: Copyright (c) 2016~2022 by wangcc, All Rights Reserved. 
 -->
@@ -11,24 +11,50 @@
   <div class="base-box">
     <breadcrumb></breadcrumb>
     <div class="_container">
-      <h3>{{$t('message.aboutUs')}}</h3>
-       <div class="content" v-html="aboutUs.dictValue">
+      <h3>{{ $t('message.aboutUs') }}</h3>
+      <video-player style="width: 100%;height: 675px;"  class="video-player vjs-custom-skin"
+        ref="videoPlayer" :playsinline="true" :options="playerOptions">
+      </video-player>
+      <div class="content" v-html="aboutUs.dictValue">
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import { getDicts } from '@/api/http';
+import { getDicts } from '@/api/http';
 export default {
   name: 'about',
   data() {
     return {
-      aboutUs:{}
+      aboutUs: {},
+      playerOptions: {
+        playbackRates: [0.5, 1.0, 1.5, 2.0], // 可选的播放速度
+        autoplay: false,  // 如果为true,浏览器准备好时开始回放
+        muted: false,     // 默认情况下将会消除任何音频。
+        loop: false,      // 是否视频一结束就重新开始。
+        preload: 'auto',  // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+        language: 'zh-CN',
+        aspectRatio: '16:9',  // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+        fluid: true,  // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+        sources: [{
+          type: "video/mp4",  // 类型
+          src: ''             // url地址
+        }],
+        poster: '',  // 封面地址
+        notSupportedMessage: '暂无视频或视频无法播放，请稍后再试',  // 允许覆盖Video.js无法播放媒体源时显示的默认信息。
+        controlBar: {
+          timeDivider: true,           // 当前时间和持续时间的分隔符
+          durationDisplay: true,       // 显示持续时间
+          remainingTimeDisplay: false, // 是否显示剩余时间功能
+          fullscreenToggle: true       // 是否显示全屏按钮
+        }
+      },
     };
   },
   created() {
-    this.getAboutus()
+    this.getAboutus();
+    this.getVideo();
   },
   methods: {
     getAboutus() {
@@ -37,6 +63,13 @@ export default {
           this.aboutUs = res.data[0];
         }
       });
+    },
+    getVideo() {
+      getDicts('about_video').then((res) =>{
+        if (res.code == 200) {
+          this.playerOptions.sources[0].src =  res.data[0].dictValue;
+        }
+      })
     }
   }
 };
